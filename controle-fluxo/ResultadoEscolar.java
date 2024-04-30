1885c8345c58e939ea0f9e12d2fa366808ef815a
vallzin <valmilson@gmail.com>
2024-04-30 16:43:01 -0300

    criada uma classe para testar uma condicional composta

---
+++ controle-fluxo/ResultadoEscolar.java
@@ -0,0 +1,8 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota >= 7)
+        System.out.println("Aprovado");
+    }
+}
--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,5 +1,10 @@
 public class CaixaEletronico {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        double saldo = 25.00;
+        double valorSolicitado = 26.00;
+
+        if(valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+            System.out.println(saldo);
     }
 }
