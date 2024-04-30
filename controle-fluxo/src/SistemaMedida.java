fd788ae8f7a8c8ddb1cfcf9fdfbc0666e8865fca
vallzin <valmilson@gmail.com>
2024-04-30 19:12:57 -0300

    adicionada a classe SistemaMedida com switch case

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,20 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "P";
+
+        switch (sigla) {
+            case "P":
+                System.out.println("Pequeno");
+                break;
+            case "M":
+                System.out.println("Médio");
+                break;
+            case "G":
+                System.out.println("Grande");
+                break;
+            default:
+                System.out.println("Sigla indefinida");
+                break;
+        }
+    }
+}
