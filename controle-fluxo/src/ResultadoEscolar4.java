7f4c44fb1b17253618256353de2a101be28b3f40
vallzin <valmilson@gmail.com>
2024-04-30 18:57:09 -0300

    adicionada a classe ResultadoEscolhar4 condição 1 como exemplo de condição ternária

---
+++ controle-fluxo/src/ResultadoEscolar4.java
@@ -0,0 +1,8 @@
+public class ResultadoEscolar4 {
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+    }
+}
